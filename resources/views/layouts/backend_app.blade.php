268694e7662a09a0f8c700482a6c42df511d0687
Md Iqbal Hossain <59364699+iqbal17115@users.noreply.github.com>
2022-11-07 00:19:38 +0600

    Updated

---
+++ resources/views/layouts/backend_app.blade.php
@@ -0,0 +1,75 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8" />
+    <title>Dashboard | Skote - Responsive Bootstrap 4 Admin Dashboard</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta content="Premium Multipurpose Admin & Dashboard Template" name="description" />
+    <meta content="Themesbrand" name="author" />
+    <!-- App favicon -->
+    <link rel="shortcut icon" href="assets/images/favicon.ico">
+
+    <!-- Bootstrap Css -->
+    <link href="assets/css/bootstrap.min.css" id="bootstrap-style" rel="stylesheet" type="text/css" />
+    <!-- Icons Css -->
+    <link href="assets/css/icons.min.css" rel="stylesheet" type="text/css" />
+    <!-- App Css-->
+    <link href="assets/css/app.min.css" id="app-style" rel="stylesheet" type="text/css" />
+    @yield('css')
+
+    @include('layouts.head-scripts')
+
+</head>
+
+<body data-sidebar="dark">
+
+    <div id="layout-wrapper">
+        @include('layouts.topbar')
+        @include('layouts.sidebar')
+        <!-- ============================================================== -->
+        <!-- Start right Content here -->
+        <!-- ============================================================== -->
+        <div class="main-content">
+            <div class="page-content">
+                <div class="container-fluid">
+                    @yield('content')
+                </div>
+                <!-- container-fluid -->
+            </div>
+            <!-- End Page-content -->
+            @include('layouts.footer')
+        </div>
+        <!-- end main content-->
+    </div>
+    <!-- END layout-wrapper -->
+
+    <!-- Right Sidebar -->
+    @include('layouts.right-sidebar')
+    <!-- /Right-bar -->
+
+    <!-- Right bar overlay-->
+    <div class="rightbar-overlay"></div>
+
+    <!-- JAVASCRIPT -->
+    <script src="{{ URL::asset('assets/libs/jquery/jquery.min.js') }}"></script>
+    <script src="{{ URL::asset('assets/libs/bootstrap/js/bootstrap.bundle.min.js') }}"></script>
+    <script src="{{ URL::asset('assets/libs/metismenu/metisMenu.min.js') }}"></script>
+    <script src="{{ URL::asset('assets/libs/simplebar/simplebar.min.js') }}"></script>
+    <script src="{{ URL::asset('assets/libs/node-waves/waves.min.js') }}"></script>
+
+    <!-- apexcharts -->
+    <script src="{{ URL::asset('assets/libs/apexcharts/apexcharts.min.js') }}"></script>
+
+    <script src="{{ URL::asset('assets/js/pages/dashboard.init.js') }}"></script>
+
+    <!-- App js -->
+    <script src="assets/js/app.js"></script>
+
+    <!-- App js -->
+    <script src="{{ URL::asset('assets/js/app.min.js')}}"></script>
+    @yield('script')
+    @stack('script')
+</body>
+
+</html>
