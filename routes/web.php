421988aed57b74a66f6f546217b12ccc2e57f268
Md Iqbal Hossain <59364699+iqbal17115@users.noreply.github.com>
2022-11-08 23:17:36 +0600

    Updated

--- routes/web.php
+++ routes/web.php
@@ -7,84 +7,6 @@ use App\Http\Controllers\Frontend\CheckoutController;
 use App\Http\Controllers\UserController;
 use App\Http\Controllers\FrontEnt\LoginController;
 use App\Http\Controllers\ReportController;
-use App\Http\Livewire\Backend\ContactInfo\ContactCategory;
-use App\Http\Livewire\Backend\ContactInfo\Customer as CustomerInfo;
-use App\Http\Livewire\Backend\ContactInfo\Staff;
-use App\Http\Livewire\Backend\ContactInfo\Supplier;
-use App\Http\Livewire\Backend\Inventory\Purchase;
-use App\Http\Livewire\Backend\Inventory\PurchaseInvoice;
-use App\Http\Livewire\Backend\Inventory\PurchaseList;
-use App\Http\Livewire\Backend\Inventory\Sale;
-use App\Http\Livewire\Backend\Inventory\SaleInvoice;
-use App\Http\Livewire\Backend\Inventory\SaleList;
-use App\Http\Livewire\Backend\Inventory\StockAdjustment;
-use App\Http\Livewire\Backend\Order\ProcessingOrderList;
-use App\Http\Livewire\Backend\Order\CancelOrderList;
-use App\Http\Livewire\Backend\Order\OrderInvoice;
-use App\Http\Livewire\Backend\Order\OrderList;
-use App\Http\Livewire\Backend\Order\ReturnedOrderList;
-use App\Http\Livewire\Backend\Order\PrintOrder;
-use App\Http\Livewire\Backend\Order\ShippedOrderList;
-use App\Http\Livewire\Backend\Order\DeliveredOrderList;
-use App\Http\Livewire\Backend\ProductInfo\Brand;
-use App\Http\Livewire\Backend\ProductInfo\Category;
-use App\Http\Livewire\Backend\ProductInfo\Color;
-use App\Http\Livewire\Backend\ProductInfo\Product;
-use App\Http\Livewire\Backend\ProductInfo\ProductList;
-use App\Http\Livewire\Backend\ProductInfo\Size;
-use App\Http\Livewire\Backend\ProductInfo\SubCategory;
-use App\Http\Livewire\Backend\ProductInfo\SubSubCategory;
-use App\Http\Livewire\Backend\ProductInfo\Unit;
-use App\Http\Livewire\Backend\ProductInfo\ProductFeature;
-use App\Http\Livewire\Backend\Report\CouponsReport;
-use App\Http\Livewire\Backend\Report\OrderReport;
-use App\Http\Livewire\Backend\Report\ProfitLoss;
-use App\Http\Livewire\Backend\Report\PurchaseReturnReport;
-use App\Http\Livewire\Backend\Report\SalesReturnReport;
-use App\Http\Livewire\Backend\Report\StockAdjustmentReport;
-use App\Http\Livewire\Backend\Report\StockReport;
-use App\Http\Livewire\Backend\Setting\Branch;
-use App\Http\Livewire\Backend\Setting\BreakingNews;
-use App\Http\Livewire\Backend\Setting\CompanyInfo;
-use App\Http\Livewire\Backend\Setting\CouponCode;
-use App\Http\Livewire\Backend\Setting\Currency;
-use App\Http\Livewire\Backend\Setting\DeliveryMethod;
-use App\Http\Livewire\Backend\Setting\InvoiceSetting;
-use App\Http\Livewire\Backend\Setting\PaymentMethod;
-use App\Http\Livewire\Backend\Setting\PointPolicy;
-use App\Http\Livewire\Backend\Setting\ShippingCharge;
-use App\Http\Livewire\Backend\Setting\Slider;
-use App\Http\Livewire\Backend\Setting\Vat;
-use App\Http\Livewire\Backend\Setting\Warehouse;
-use App\Http\Livewire\Backend\Setting\Language;
-use App\Http\Livewire\Backend\Setting\ManageLanguage;
-use App\Http\Livewire\Backend\ContactUs\Message;
-use App\Http\Livewire\Backend\Transaction\CustomerPayment;
-use App\Http\Livewire\Backend\Transaction\CustomerPaymentReport;
-use App\Http\Livewire\Backend\Transaction\Payment;
-use App\Http\Livewire\Backend\Transaction\SupplierPayment;
-use App\Http\Livewire\Backend\Transaction\SupplierPaymentReport;
-use App\Http\Livewire\Backend\ProductInfo\PreviewProduct;
-use App\Http\Livewire\Backend\Vendor\VendorList;
-use App\Http\Livewire\Backend\Vendor\VendorApprovedList;
-use App\Http\Livewire\Backend\Vendor\VendorCancelList;
-use App\Http\Livewire\Frontend\Category as FrontEndCategory;
-use App\Http\Livewire\Frontend\CategoryWiseProduct;
-use App\Http\Livewire\Frontend\Contact as ContactUs;
-use App\Http\Livewire\Frontend\Error;
-use App\Http\Livewire\Frontend\MyProfile;
-use App\Http\Livewire\Frontend\ReturnPolicy;
-use App\Http\Livewire\Frontend\ProductView;
-use App\Http\Livewire\Frontend\SignIn;
-use App\Http\Livewire\Frontend\SignUp;
-use App\Http\Livewire\Frontend\LogIn;
-use App\Http\Livewire\Frontend\Wishlist;
-use App\Http\Livewire\Inventory\DelieveryMethod;
-use App\Http\Livewire\UserManagement\UserList;
-use App\Http\Livewire\UserProfile\AuthLockScreen;
-use App\Http\Livewire\UserProfile\ChangePassword;
-use App\Http\Livewire\UserProfile\ProfileSettings;
-use App\Http\Livewire\UserManagement\AllUserList;
 
 use App\Http\Livewire\Backend\Offer\Offer;
 use App\Http\Livewire\Backend\Order\OrderEdit;
@@ -117,15 +39,9 @@ Route::get('checkout', [CheckoutController::class, 'index'])->name('checkout');
 Route::get('/', function () {
     return view('auth.login');
 });
-Route::group(['prefix' => 'customer'], function () {
-    // Route::get('customer_login', Customer::class)->name('customer_login');
-    Route::get('category_wise_product/{id?}', CategoryWiseProduct::class)->name('category_wise_product');
-    Route::get('product_view/{id?}', ProductView::class)->name('product_view');
-});
 
-// Route::get('privacy-policy', PrivacyPolicy::class)->name('privacy-policy');
+
 Route::get('/', [HomeController::class, 'index'])->name('home');
-// Route::get('/cart', [HomeController::class, 'cart'])->name('cart');
 Route::get('/customer_login', [HomeController::class, 'CustomerLogin'])->name('customer_login');
 Route::get('/check-out', [HomeController::class, 'checkOut'])->name('check-out');
 Route::get('/offer', [HomeController::class, 'Offer'])->name('offer');
@@ -153,150 +69,13 @@ Route::get('contact', [HomeController::class, 'Contact'])->name('contact');
 Route::get('about', [HomeController::class, 'About'])->name('about');
 Route::get('privacy-policy', [HomeController::class, 'PrivacyPolicy'])->name('privacy-policy');
 Route::get('terms-condition', [HomeController::class, 'TermsAndCondition'])->name('terms-condition');
-// Route::get('change-password', [HomeController::class, 'ChangePassword'])->name('change-password');
-// Route::get('/search-category-wise/{id?}', [HomeController::class, 'searchByCategory'])->name('search-category-wise');
-Route::get('category', FrontEndCategory::class)->name('category');
-Route::get('sign-in', SignIn::class)->name('sign-in');
-Route::get('sign-up', SignUp::class)->name('sign-up');
-Route::get('log-in', LogIn::class)->name('log-in');
-//Route::get('cart', Cart::class)->name('cart');
-//Route::get('check-out', Checkout::class)->name('check-out');
-Route::get('contact-us', ContactUs::class)->name('contact-us');
-// Route::get('terms-conditios', TermsConditios::class)->name('terms-conditios');
-Route::get('my-profile', MyProfile::class)->name('my-profile');
-Route::get('return-policy', ReturnPolicy::class)->name('return-policy');
-Route::get('message', Message::class)->name('message');
 
 
-// Route::get('about', AboutUs::class)->name('about');
-Route::get('error', Error::class)->name('error');
-// Route::get('order-completed', OrderCompleted::class)->name('order-completed');
-
-Route::get('wish-list', Wishlist::class)->name('wish-list');
 Route::Post('customer_sign_in', [LoginController::class, 'authenticate'])->name('customer_sign_in');
 Route::group(['middleware' => ['role:admin|user|manager|editor']], function () {
     Route::get('/admin', [HomeController::class, 'adminDashboard'])->name('dashboard')->middleware(['auth:sanctum', 'verified']);
 
-    // Route::get('dashboard', Dashboard::class)->name('dashboard');
-
     Route::group(['prefix' => 'member', 'middleware' => ['auth']], function () {
-        Route::group(['prefix' => 'offer', 'as' => 'offer.'], function () {
-            Route::get('offer', Offer::class)->name('offer');
-        });
-        Route::group(['prefix' => 'user-management', 'as' => 'user-management.'], function () {
-            Route::get('user-list', UserList::class)->name('user-list');
-            Route::get('user-restiction/{id?}', [UserController::class, 'userRestiction'])->name('user-restiction');
-            Route::post('user_restiction/update', [UserController::class, 'UserRectictionsUpdate'])->name('user_restictions.update');
-        });
-        Route::group(['prefix' => 'inventory', 'as' => 'inventory.'], function () {
-            Route::get('category', Category::class)->name('category');
-            Route::get('currency', Currency::class)->name('currency');
-            // Route::get('language', Language::class)->name('language');
-            Route::get('delivery-method', DelieveryMethod::class)->name('delivery-method');
-            Route::get('ware-house', WareHouse::class)->name('ware-house');
-            Route::get('purchase/{id?}', Purchase::class)->name('purchase');
-            Route::get('purchase-list', PurchaseList::class)->name('purchase-list');
-            Route::get('purchase-invoice/{id}', PurchaseInvoice::class)->name('purchase-invoice');
-            Route::get('sale/{id?}', Sale::class)->name('sale');
-            Route::get('sale-list', SaleList::class)->name('sale-list');
-            Route::get('sale-invoice/{id}', SaleInvoice::class)->name('sale-invoice');
-            Route::get('stock-adjustment', StockAdjustment::class)->name('stock-adjustment');
-        });
-        Route::group(['prefix' => 'user-profile', 'as' => 'user-profile.'], function () {
-            Route::get('profile-settings', ProfileSettings::class)->name('profile-settings');
-            Route::get('change-password', ChangePassword::class)->name('change-password');
-            Route::get('auth-lock-screen', AuthLockScreen::class)->name('auth-lock-screen');
-        });
-
-        Route::group(['prefix' => 'product', 'as' => 'product.'], function () {
-            Route::get('category', Category::class)->name('category');
-            Route::get('sub-category', SubCategory::class)->name('sub-category');
-            Route::get('brand', Brand::class)->name('brand');
-            Route::get('product/{id?}', Product::class)->name('product');
-            Route::get('product-list', ProductList::class)->name('product-list');
-            Route::get('preview-product/{id?}', PreviewProduct::class)->name('preview-product');
-            Route::get('sub-sub-category', SubSubCategory::class)->name('sub-sub-category');
-            Route::get('product-feature', ProductFeature::class)->name('product-feature');
-            Route::get('unit', Unit::class)->name('unit');
-            Route::get('color', Color::class)->name('color');
-            Route::get('size', Size::class)->name('size');
-        });
-
-        Route::group(['prefix' => 'setting', 'as' => 'setting.'], function () {
-            Route::get('companyinfo', CompanyInfo::class)->name('companyinfo');
-            Route::get('branch', Branch::class)->name('branch');
-            Route::get('currency', Currency::class)->name('currency');
-            Route::get('delivery-method', DeliveryMethod::class)->name('delivery-method');
-            Route::get('invoice-setting', InvoiceSetting::class)->name('invoice-setting');
-            Route::get('payment-method', PaymentMethod::class)->name('payment-method');
-            Route::get('coupon-code', CouponCode::class)->name('coupon-code');
-            Route::get('vat', Vat::class)->name('vat');
-            Route::get('shipping-charge', ShippingCharge::class)->name('shipping-charge');
-            Route::get('warehouse', Warehouse::class)->name('warehouse');
-            Route::get('slider', Slider::class)->name('slider');
-            Route::get('point-policy', PointPolicy::class)->name('point-policy');
-            Route::get('breaking-news', BreakingNews::class)->name('breaking-news');
-            Route::get('language', Language::class)->name('language');
-            Route::get('manage-language/{id?}', ManageLanguage::class)->name('manage-language');
-        });
-
-        Route::group(['prefix' => 'order',  'as' => 'order.'], function () {
-            Route::get('order-list', OrderList::class)->name('order-list');
-            Route::get('order-processing', ProcessingOrderList::class)->name('order-processing');
-            Route::get('order-shipped', ShippedOrderList::class)->name('order-shipped');
-            Route::get('order-delivered', DeliveredOrderList::class)->name('order-delivered');
-            Route::get('order-returned', ReturnedOrderList::class)->name('order-returned');
-            Route::get('order-cancel', CancelOrderList::class)->name('order-cancel');
-            Route::get('print-order', PrintOrder::class)->name('print-order');
-            Route::get('order-edit/{id?}', OrderEdit::class)->name('order-edit');
-        });
-
-        Route::group(['prefix' => 'transaction', 'as' => 'transaction.'], function () {
-            Route::get('payment', Payment::class)->name('payment');
-            Route::get('customer-payment/{id?}', CustomerPayment::class)->name('customer-payment');
-            Route::get('supplier-payment/{id?}', SupplierPayment::class)->name('supplier-payment');
-            Route::get('customer-payment-report', CustomerPaymentReport::class)->name('customer-payment-report');
-            Route::get('supplier-payment-report', SupplierPaymentReport::class)->name('supplier-payment-report');
-        });
-        Route::group(['prefix' => 'order',  'as' => 'order.'], function () {
-            // Route::get('order-list/{id?}', OrderList::class)->name('order-list');
-            Route::get('order-invoice/{id}', OrderInvoice::class)->name('order-invoice');
-            // Route::get('order-cancel', CancelOrderList::class)->name('order-cancel');
-            // Route::get('print-order', PrintOrder::class)->name('print-order');
-        });
-
-        Route::group(['prefix' => 'contact-info', 'as' => 'contact-info.'], function () {
-            Route::get('contact-category', ContactCategory::class)->name('contact-category');
-            Route::get('customer', CustomerInfo::class)->name('customer');
-            Route::get('supplier', Supplier::class)->name('supplier');
-            Route::get('staff', Staff::class)->name('staff');
-            Route::get('all-user-list', AllUserList::class)->name('all-user-list');
-        });
-
-        Route::group(['prefix' => 'report', 'as' => 'report.'], function () {
-            Route::get('stock-report', StockReport::class)->name('stock-report');
-            Route::get('order-report', OrderReport::class)->name('order-report');
-        });
-
-        Route::group(['prefix' => 'vendor', 'as' => 'vendor.'], function () {
-            Route::get('vendor-list', VendorList::class)->name('vendor-list');
-            Route::get('vendor-approved-list', VendorApprovedList::class)->name('vendor-approved-list');
-            Route::get('vendor-cancel-list', VendorCancelList::class)->name('vendor-cancel-list');
-        });
-
-        Route::group(['prefix' => 'data', 'as' => 'data.'], function () {
-            Route::get('index', [DatatableController::class, 'index'])->name('index');
-
-            Route::get('user-table', [DatatableController::class, 'UserTable'])->name('user_table');
-        });
-
-        Route::group(['prefix' => 'report', 'as' => 'report.'], function () {
-            Route::get('stock-adjustment-report', StockAdjustmentReport::class)->name('stock-adjustment-report');
-            Route::get('purchase-return-report', PurchaseReturnReport::class)->name('purchase-return-report');
-            Route::get('sales-return-report', SalesReturnReport::class)->name('sales-return-report');
-            Route::get('coupons-report', CouponsReport::class)->name('coupons-report');
-            Route::get('profit-loss', ProfitLoss::class)->name('profit-loss');
-        });
 
         // Start Report
         Route::group(['prefix' => 'report', 'as' => 'report.'], function () {
