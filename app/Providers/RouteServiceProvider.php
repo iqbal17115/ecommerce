27aa4da13336c2d594b72ca32072815e82ec2534
Md Iqbal Hossain <59364699+iqbal17115@users.noreply.github.com>
2023-03-14 22:06:41 +0600

    Login Redirect

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -17,7 +17,7 @@ class RouteServiceProvider extends ServiceProvider
      *
      * @var string
      */
-    public const HOME = '/my-account';
+    public const HOME = '/admin';
 
     /**
      * The controller namespace for the application.
