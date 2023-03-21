ed87d3b489dab2846f6d79de49c2cf20aa60f23c
Md Iqbal Hossain <59364699+iqbal17115@users.noreply.github.com>
2023-03-22 00:22:44 +0600

    Update header.blade.php

--- resources/views/ecommerce/header.blade.php
+++ resources/views/ecommerce/header.blade.php
@@ -33,7 +33,7 @@
             @if($company_info && $company_info->logo)
             <a href="{{ route('home') }}" class="logo">
                 <img src="{{ asset('storage/'.$company_info->logo) }}" class="w-100" width="111" height="44"
-                    style="height: 151px;" alt="Porto Logo">
+                    style="height: 44px;width: 151px;" alt="Porto Logo">
             </a>
             @endif
 
