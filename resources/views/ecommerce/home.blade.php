8a22ebeaf31f2fc6678784ee4c6a7b58019d3f37
Md Iqbal Hossain <59364699+iqbal17115@users.noreply.github.com>
2023-03-28 00:18:58 +0600

    Auto Play Category Slide

--- resources/views/ecommerce/home.blade.php
+++ resources/views/ecommerce/home.blade.php
@@ -31,6 +31,8 @@
 
             <div class="categories-section appear-animate" data-animation-name="fadeIn" data-animation-delay="100">
                 <div class="categories-slider owl-carousel owl-theme show-nav-hover nav-outer" data-owl-options="{
+                            'autoplay': true, 
+                            'autoplayTimeout': 3000,
                             'responsive': {
                                 '0': {
                                     'items': 2
