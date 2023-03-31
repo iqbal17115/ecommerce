62aac8bb12e2ff546bbb4f2523fbbbaa2fc5376e
Md Iqbal Hossain <59364699+iqbal17115@users.noreply.github.com>
2023-04-01 02:05:27 +0600

    Update paginate-shop.blade.php

--- resources/views/ecommerce/paginate-shop.blade.php
+++ resources/views/ecommerce/paginate-shop.blade.php
@@ -17,13 +17,8 @@
                 </h3>
                 <!-- End .product-container -->
                 <div class="price-box">
-                @php
-                                echo $product->your_price? '<span
-                                    class="old-price">'.$currency->icon.''.number_format((float)$product->your_price, 2).'</span>' : '';
-                                echo $product->sale_price? '<span
-                                    class="product-price">'.$currency->icon.''.number_format((float)$product->sale_price, 2).'</span>' :
-                                '';
-                                @endphp
+                    <span class="old-price">$29.00</span>
+                    <span class="product-price">$19.00</span>
                 </div>
                 <!-- End .price-box -->
             </div>
