a6149cd258bd6959f7e751a86a5d3e51c76f15b0
Md Iqbal Hossain <59364699+iqbal17115@users.noreply.github.com>
2023-04-14 21:36:07 +0600

    Update home.blade.php

--- resources/views/ecommerce/home.blade.php
+++ resources/views/ecommerce/home.blade.php
@@ -23,7 +23,7 @@
 			}">
                 @foreach($sliders as $slider)
                 <div class="home-slide home-slide1 banner">
-                    <img class="slider_image slide-bg" src="{{ asset('storage/'.$slider->image) }}" alt="slider image">
+                    <img class="slider_image slide-bg" src="{{ asset('storage/'.$slider->image) }}" alt="slider image" style="min-height: 208px;">
                     <div
                         class="container d-flex align-items-sm-center justify-content-sm-between justify-content-center flex-column flex-sm-row">
                         <div class="banner-content content-left text-sm-right mb-sm-0 mb-2"></div>
