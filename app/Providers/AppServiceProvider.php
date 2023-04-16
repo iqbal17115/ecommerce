5a63a742eec901aaf2d824bf825651009933c670
Md Iqbal Hossain <59364699+iqbal17115@users.noreply.github.com>
2023-04-17 00:50:06 +0600

    Update AppServiceProvider.php

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,12 +4,10 @@ namespace App\Providers;
 
 use App\Models\Backend\Currency\Currency;
 use App\Models\Backend\Product\Category;
-use App\Models\Backend\WebSetting\Advertisement;
 use App\Models\Backend\WebSetting\CompanyInfo;
 use Illuminate\Pagination\Paginator;
 use Illuminate\Support\Facades\View;
 use Illuminate\Support\ServiceProvider;
-use DB;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -30,20 +28,13 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
         Paginator::useBootstrap();
-        //Categories rrrr
+        //Categories
         View::composer('*', function ($view) {
             $view->with('parentCategories', Category::whereParentCategoryId(null)->whereTopMenu(1)->orderBy('position', 'asc')->get());
             $view->with('sidebarMenuCategories', Category::whereSidebarMenu(1)->orderByRaw('ISNULL(sidebar_menu_position), sidebar_menu_position ASC')->get());
             $view->with('headerMenuCategories', Category::whereHeaderMenu(1)->orderByRaw('ISNULL(header_menu_position), header_menu_position ASC')->get());
             $view->with('company_info', CompanyInfo::first());
             $view->with('currency', Currency::whereIsDefault(1)->first());
-            $view->with('all_active_advertisements', Advertisement::orderBy('position')->whereIsActive(1)
-            ->get()
-            ->groupBy('page')
-            ->map(function ($ads, $page) {
-                return collect($ads)->keyBy('position');
-            })
-            ->toArray());
         });
 
         
