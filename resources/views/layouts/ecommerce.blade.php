16a12b922e5f3519105e6628a2823123ea8a435c
Md Iqbal Hossain <59364699+iqbal17115@users.noreply.github.com>
2023-04-17 19:58:31 +0600

    Remove Cart Link

--- resources/views/layouts/ecommerce.blade.php
+++ resources/views/layouts/ecommerce.blade.php
@@ -223,21 +223,6 @@
             e.preventDefault();
             $("#sidebar-wrapper").css("width", "320px");
             $("#wrapper").toggleClass("menuDisplayed");
-            // Call Sidebar Content
-            //    $.ajax({
-            //     url: "{{route('get_sidebar_content')}}",
-            //     method: 'get',
-            //     async: false,
-            //     success: function(data) {
-            //         console.log(data);
-            //         $('#category_show').html(data);
-            //     },
-            //     error: function(err) {
-            //         var error = err.responseJSON;
-            //         console.log(error);
-
-            //     }
-            // });
         });
     });
     </script>
