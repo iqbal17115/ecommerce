7ea1a98db43495daad11ac58a821586df9e00e0a
md alamin <55638374+mdalmin291@users.noreply.github.com>
2023-05-22 23:18:12 +0600

    updated

--- resources/views/layouts/ecommerce.blade.php
+++ resources/views/layouts/ecommerce.blade.php
@@ -8,18 +8,52 @@
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
     <title>Aladdinne</title>
+    @if($company_info && $company_info->key_word)
+    <meta name="keywords" content="{{ $company_info->key_word }}">
+   @endif
 
-    <meta name="keywords" content="HTML5 Template" />
-    <meta name="description" content="Aladdinne">
-    <meta name="author" content="SW-THEMES">
-    <meta name="csrf-token" content="{{ csrf_token() }}">
+   @if($company_info && $company_info->description)
+    <meta name="description" content="{{ $company_info->description }}">
+   @endif
 
+   <meta name="author" content="SW-THEMES">
+   <link rel="canonical" href="https://www.aladdinne.com">
+   <meta name="csrf-token" content="{{ csrf_token() }}">
+    <script type='application/ld+json'>
+        {
+          "@context": "http://www.schema.org",
+          "@type": "ShoppingCenter",
+          "name": "Aladdinne.com",
+          "url": "https://www.aladdinne.com/",
+          "logo": "https://www.aladdinne.com/storage/company_info/logos/Lpzoe54rj1p8i4K6mpwVRIxdPmS6CSfP59liCSyR.png",
+          "image": "https://www.aladdinne.com/storage/company_info/logos/Lpzoe54rj1p8i4K6mpwVRIxdPmS6CSfP59liCSyR.png",
+          "description": "Aladdinne is an e-commerce marketplace platform that connects multiple buyers and sellers to facilitate buyers and sellers both by the buying and selling of a wide range of products and services in our marketplace, such as big brands selling items ranging from electronics, Fashion, home Decor & appliances, gadgets, garden supplies, groceries, etc. Aladdinne provides service through the aladdinne.com website, mobile app, and other platforms.",
+          "address": {
+            "@type": "PostalAddress",
+            "streetAddress": "Office No:523, 5th Floor, Alpona Plaza",
+            "addressLocality": "Dhaka",
+            "addressRegion": "Dhaka",
+            "postalCode": "1205",
+            "addressCountry": "Bangladesh"
+          },
+          "geo": {
+            "@type": "GeoCoordinates",
+            "latitude": "23.7386671",
+            "longitude": "90.381785"
+          },
+          "hasMap": "https://www.google.com/maps/search/https:%2F%2Fwww.google.com%2Fmaps%2Fplace%2FAladdinne.com%2F@23.7386622,90.3843599,15z%2Fdata%3D!4m6!3m5!1s0x3755b9ecc12ae8c5:0x29d5b7fd67107fe5!8m2!3d23.7386622!4d90.3843599!16s%252Fg%252F11tcg4q1dx/@23.7386671,90.381785,17z/data=!3m1!4b1",
+          "openingHours": "Mo, Tu, We, Th, Fr, Sa, Su 08:00-17:00",
+          "contactPoint": {
+            "@type": "ContactPoint",
+            "contactType": "support @aladdinne.com",
+            "telephone": "+8801975340753"
+          }
+        }
+    </script>
     <!-- Favicon -->
     @if ($company_info && $company_info->icon)
         <link rel="icon" type="image/x-icon" href="{{ asset('storage/' . $company_info->icon) }}">
     @endif
-
-
    @include('layouts.parts.css-links')
 
 </head>
