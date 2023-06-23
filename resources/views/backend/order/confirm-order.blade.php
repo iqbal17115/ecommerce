b074f495b35ad9becf85ffc703d84384de9dc517
Md Iqbal Hossain <59364699+iqbal17115@users.noreply.github.com>
2023-06-23 15:12:40 +0600

    Update confirm-order.blade.php

--- resources/views/backend/order/confirm-order.blade.php
+++ resources/views/backend/order/confirm-order.blade.php
@@ -74,7 +74,7 @@
                     @foreach ($order->OrderDetail as $orderDetail)
                     <div class="row">
                         <div class="col-md-1">
-                            <img src="{{ asset('storage/'.$orderDetail->Product?->ProductImage?->first()->image) }}" class="img-responsive">
+                            <img src="{{ asset('storage/product_photo/'.$orderDetail->Product?->ProductImage?->first()->image) }}" class="img-responsive">
                         </div>
                         <div class="col-md-7">
                             <h6 class="mb-3">{{ $orderDetail->Product->name }}</h6>
