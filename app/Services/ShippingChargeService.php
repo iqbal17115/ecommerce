6778bc4531ebb7025c4a809b829cd54e5b9bf4ae
Md Iqbal Hossain <59364699+iqbal17115@users.noreply.github.com>
2023-07-30 22:34:09 +0600

    Shipping Charge

--- app/Services/ShippingChargeService.php
+++ app/Services/ShippingChargeService.php
@@ -33,13 +33,13 @@ class ShippingChargeService
     private function calculateDimensionalWeight(float $length, float $width, float $height, $dimensionalWeightFactor): float
     {
         $dimensionalWeight = ($this->convertLengthTo($length, 'm', 'cm') * $this->convertLengthTo($width, 'm', 'cm') * $this->convertLengthTo($height, 'm', 'cm')) / 5000;
-        $dimensionalWeightFactor = $this->convertWeightTo($dimensionalWeightFactor, 'gm', 'kg');
+        $dimensionalWeight = $this->convertWeightTo($dimensionalWeight, 'kg', 'gm');
 
         if($dimensionalWeightFactor > $dimensionalWeight) {
            $dimensionalWeight = $dimensionalWeightFactor;
         }
 
-        return $this->convertWeightTo($dimensionalWeight, 'kg', 'gm');
+        return $dimensionalWeight;
     }
 
     private function findMatchingClass(float $dimensionalWeight): ?array
@@ -80,11 +80,11 @@ class ShippingChargeService
         $products = session('cart');
         $totalAmount = 0; // Initialize total amount to 0
         $totalShippingCharge = 0;
-
+        $shippingMethodId = 'ee1f0de6-223e-11ee-aaf7-5811220534bb';
         foreach ($products as $productId => $productData) {
             $product = Product::find($productId);
             $quantity = $productData['quantity'];
-            $shippingMethodId = 'ee1f0de6-223e-11ee-aaf7-5811220534bb';
+
 
             $totalAmount = $quantity * $product->sale_price; // Total Amount
 
@@ -106,6 +106,7 @@ class ShippingChargeService
 
             // Get the class that matches the criteria
             $matchingClass = $this->findMatchingClass($dimensionalWeight);
+
             if (!$matchingClass) {
                 $matchingClass['name'] = null;
             }
@@ -115,27 +116,20 @@ class ShippingChargeService
             //     $matchingClass = $this->getDefaultClass();
             // }
             // Calculate regular shipping charges
-            $charge = ShippingCharge::where('shipping_method_id', $shippingMethodId)
-                ->where('shipping_class', $matchingClass['name'])
-                ->where('from_weight', '<=', $dimensionalWeight)
-                ->where('to_weight', '>=', $dimensionalWeight)
-                ->where(function ($query) use ($quantity, $totalAmount) {
-                    $query->where(function ($query) use ($quantity, $totalAmount) {
-                        $query->whereNull('min_quantity')
-                            ->whereNull('max_quantity');
-                    })->orWhere(function ($query) use ($quantity, $totalAmount) {
-                        $query->where('min_quantity', '<=', $quantity)
-                            ->where('max_quantity', '>=', $quantity);
-                    });
-                })
-                ->orderBy('from_weight')
-                ->first();
+            $shipping_charge = ShippingCharge::where('shipping_method_id', $shippingMethodId)
+            ->where('shipping_class', $matchingClass['name'])
+            ->where('from_weight', '<=', $dimensionalWeight)
+            ->where('to_weight', '>=', $dimensionalWeight)
+            ->where('min_quantity', '<=', $quantity)
+            ->where('max_quantity', '>=', $quantity)
+            ->orderBy('from_weight')
+            ->first();
 
-            if ($charge) {
-                $totalShippingCharge += $charge->charge;
+            if ($shipping_charge) {
+                $totalShippingCharge += $shipping_charge->charge;
             } else {
-        dd($charge, $dimensionalWeight);
 
+                // dd($shippingMethodId, $quantity, $shipping_charge, $dimensionalWeight, $matchingClass['name']);
                 // Handle the case where no shipping charge is found for a product
                 // return response()->json(['error' => 'No shipping charge found for one or more products.']);
             }
