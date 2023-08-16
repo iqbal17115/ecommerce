98ff551e0dfc5e947b5d296aa1524f98e132411c
Md Iqbal Hossain <59364699+iqbal17115@users.noreply.github.com>
2023-08-17 02:01:15 +0600

    Update shipping_charge.js

--- public/backend_js/shipping_charge.js
+++ public/backend_js/shipping_charge.js
@@ -1,7 +1,6 @@
 // Assume you have a function to fetch cart data and make the AJAX request
 function calculateShippingCharges(shipping_method_id = '13eee98c-31ed-11ee-be5c-5811220534bb') {
     // ... Code to fetch cart data and construct the data object for the request
-
     // Make the AJAX request
     $.ajax({
         url: '/calculate-shipping-charge', // Your Laravel route to the controller method
@@ -11,14 +10,8 @@ function calculateShippingCharges(shipping_method_id = '13eee98c-31ed-11ee-be5c-
             'shipping_method_id': shipping_method_id
         },
         success: function(response) {
-            // The AJAX request was successful
-            // Get the totalShippingCharge from the response JSON
-            console.log(response.charge);
-            // Set shipping charge
-            $(".shipping_amount").text(response.charge);
-            updateTotalPrice(response.charge);
-            // var totalAmount = parseFloat($(".cart_total_price:first").text()) + response.charge;
-            // $(".total-price").text(totalAmount);
+            // The AJAX request was successful;
+            updateTotalPrice(response.sub_total, response.charge);
         },
         error: function(xhr, status, error) {
             // Handle AJAX error, if any
@@ -26,3 +19,5 @@ function calculateShippingCharges(shipping_method_id = '13eee98c-31ed-11ee-be5c-
         },
     });
 }
+///
+
