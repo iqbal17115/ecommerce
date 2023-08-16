e6ced33a03a79358c2990ea8b2c582701217030c
Md Iqbal Hossain <59364699+iqbal17115@users.noreply.github.com>
2023-08-17 01:52:15 +0600

    Update shipping_charge_checkout.js

--- public/backend_js/shipping_charge_checkout.js
+++ public/backend_js/shipping_charge_checkout.js
@@ -1,7 +1,6 @@
 // Assume you have a function to fetch cart data and make the AJAX request
 function calculateShippingCharges(shipping_method_id = '13eee98c-31ed-11ee-be5c-5811220534bb') {
     // ... Code to fetch cart data and construct the data object for the request
-alert(1);
     // Make the AJAX request
     $.ajax({
         url: '/calculate-shipping-charge', // Your Laravel route to the controller method
