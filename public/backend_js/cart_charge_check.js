8e944a13816e65066dde07f29d15064c131be1dc
Md Iqbal Hossain <59364699+iqbal17115@users.noreply.github.com>
2023-08-17 01:40:12 +0600

    Update cart_charge_check.js

--- public/backend_js/cart_charge_check.js
+++ public/backend_js/cart_charge_check.js
@@ -82,7 +82,7 @@ $(document).ready(function () {
 });
 
 function updateTotalPrice(sub_total, shipping_charge) {
-    alert(shipping_charge);
+    alert(sub_total);
 
     $('.cart_total_price').text(sub_total.toFixed(0)); // Assuming you want to show two decimal places
      $(".shipping_amount").text(shipping_charge);
