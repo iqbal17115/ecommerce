f1c02824aaeef9d6183bdcb107ae77890baf13db
Md Iqbal Hossain <59364699+iqbal17115@users.noreply.github.com>
2023-09-07 01:35:13 +0600

    Update advance-edit.blade.php

--- resources/views/backend/order/advance-edit.blade.php
+++ resources/views/backend/order/advance-edit.blade.php
@@ -390,7 +390,8 @@
                                                 <div class="col-md-6">
                                                     <div class="form-group">
                                                         <label for="pickup_day">Pickup Day</label>
-                                                        <input type="date" id="pickup_day" name="pickup_day" value="{{ $order?->orderProductBox?->first()['pickup_day'] ?? '' }}"
+                                                        <input type="date" id="pickup_day" name="pickup_day"
+                                                            value="{{ $order?->orderProductBox?->first()['pickup_day'] ?? '' }}"
                                                             class="form-control" required>
                                                     </div>
                                                 </div>
@@ -399,42 +400,79 @@
                                                         <label for="pickup_time" required>Pickup Time</label>
                                                         <select id="pickup_time" name="pickup_time" class="form-control">
                                                             <option value="">-- Select --</option>
-                                                            <option value="10:00 AM - 11:00 AM" {{ isset($order?->orderProductBox?->first()['pickup_time']) == '10:00 AM - 11:00 AM' ? 'selected' : '' ?? '' }}>10:00 AM - 11:00 AM
+                                                            <option value="10:00 AM - 11:00 AM"
+                                                                {{ isset($order?->orderProductBox?->first()['pickup_time']) == '10:00 AM - 11:00 AM' ? 'selected' : '' ?? '' }}>
+                                                                10:00 AM - 11:00 AM
                                                             </option>
-                                                            <option value="10:30 AM - 11:30 AM" {{ isset($order?->orderProductBox?->first()['pickup_time']) =='10:30 AM - 11:30 AM' ? 'selected' : '' ?? '' }}>10:30 AM - 11:30 AM
+                                                            <option value="10:30 AM - 11:30 AM"
+                                                                {{ isset($order?->orderProductBox?->first()['pickup_time']) == '10:30 AM - 11:30 AM' ? 'selected' : '' ?? '' }}>
+                                                                10:30 AM - 11:30 AM
                                                             </option>
-                                                            <option value="11:00 AM - 12:00 PM" {{ isset($order?->orderProductBox?->first()['pickup_time']) =='11:00 AM - 12:00 PM' ? 'selected' : '' ?? '' }}>11:00 AM - 12:00 PM
+                                                            <option value="11:00 AM - 12:00 PM"
+                                                                {{ isset($order?->orderProductBox?->first()['pickup_time']) == '11:00 AM - 12:00 PM' ? 'selected' : '' ?? '' }}>
+                                                                11:00 AM - 12:00 PM
                                                             </option>
-                                                            <option value="11:30 AM - 12:30 PM" {{ isset($order?->orderProductBox?->first()['pickup_time']) =='11:30 AM - 12:30 PM' ? 'selected' : '' ?? '' }}>11:30 AM - 12:30 PM
+                                                            <option value="11:30 AM - 12:30 PM"
+                                                                {{ isset($order?->orderProductBox?->first()['pickup_time']) == '11:30 AM - 12:30 PM' ? 'selected' : '' ?? '' }}>
+                                                                11:30 AM - 12:30 PM
                                                             </option>
-                                                            <option value="12:00 PM - 1:00 PM" {{ isset($order?->orderProductBox?->first()['pickup_time']) =='12:00 PM - 1:00 PM' ? 'selected' : '' ?? '' }}>12:00 PM - 1:00 PM</option>
-                                                            <option value="12:30 PM - 1:30 PM" {{ isset($order?->orderProductBox?->first()['pickup_time']) =='12:30 PM - 1:30 PM' ? 'selected' : '' ?? '' }}>12:30 PM - 1:30 PM</option>
-                                                            <option value="1:00 PM - 2:00 PM" {{ isset($order?->orderProductBox?->first()['pickup_time']) =='1:00 PM - 2:00 PM' ? 'selected' : '' ?? '' }}>1:00 PM - 2:00 PM</option>
-                                                            <option value="1:30 PM - 2:30 PM" {{ isset($order?->orderProductBox?->first()['pickup_time']) =='1:30 PM - 2:30 PM' ? 'selected' : '' ?? '' }}>1:30 PM - 2:30 PM</option>
-                                                            <option value="2:00 PM - 3:00 PM" {{ isset($order?->orderProductBox?->first()['pickup_time']) =='2:00 PM - 3:00 PM' ? 'selected' : '' ?? '' }}>2:00 PM - 3:00 PM</option>
-                                                            <option value="2:30 PM - 3:30 PM" {{ isset($order?->orderProductBox?->first()['pickup_time']) =='2:30 PM - 3:30 PM' ? 'selected' : '' ?? '' }}>2:30 PM - 3:30 PM</option>
-                                                            <option value="3:00 PM - 4:00 PM" {{ isset($order?->orderProductBox?->first()['pickup_time']) =='3:00 PM - 4:00 PM' ? 'selected' : '' ?? '' }}>3:00 PM - 4:00 PM</option>
-                                                            <option value="3:30 PM - 4:30 PM" {{ isset($order?->orderProductBox?->first()['pickup_time']) =='3:30 PM - 4:30 PM' ? 'selected' : '' ?? '' }}>3:30 PM - 4:30 PM</option>
-                                                            <option value="4:00 PM - 5:00 PM" {{ isset($order?->orderProductBox?->first()['pickup_time']) =='4:00 PM - 5:00 PM' ? 'selected' : '' ?? '' }}>4:00 PM - 5:00 PM</option>
-                                                            <option value="4:30 PM - 5:30 PM" {{ isset($order?->orderProductBox?->first()['pickup_time']) =='4:30 PM - 5:30 PM' ? 'selected' : '' ?? '' }}>4:30 PM - 5:30 PM</option>
-                                                            <option value="5:00 PM - 6:00 PM" {{ isset($order?->orderProductBox?->first()['pickup_time']) =='5:00 PM - 6:00 PM' ? 'selected' : '' ?? '' }}>5:00 PM - 6:00 PM</option>
-                                                            <option value="5:30 PM - 6:30 PM" {{ isset($order?->orderProductBox?->first()['pickup_time']) =='5:30 PM - 6:30 PM' ? 'selected' : '' ?? '' }}>5:30 PM - 6:30 PM</option>
-                                                            <option value="6:00 PM - 7:00 PM" {{ isset($order?->orderProductBox?->first()['pickup_time']) =='6:00 PM - 7:00 PM' ? 'selected' : '' ?? '' }}>6:00 PM - 7:00 PM</option>
-                                                            <option value="6:30 PM - 7:30 PM" {{ isset($order?->orderProductBox?->first()['pickup_time']) =='6:30 PM - 7:30 PM' ? 'selected' : '' ?? '' }}>6:30 PM - 7:30 PM</option>
+                                                            <option value="12:00 PM - 1:00 PM"
+                                                                {{ isset($order?->orderProductBox?->first()['pickup_time']) == '12:00 PM - 1:00 PM' ? 'selected' : '' ?? '' }}>
+                                                                12:00 PM - 1:00 PM</option>
+                                                            <option value="12:30 PM - 1:30 PM"
+                                                                {{ isset($order?->orderProductBox?->first()['pickup_time']) == '12:30 PM - 1:30 PM' ? 'selected' : '' ?? '' }}>
+                                                                12:30 PM - 1:30 PM</option>
+                                                            <option value="1:00 PM - 2:00 PM"
+                                                                {{ isset($order?->orderProductBox?->first()['pickup_time']) == '1:00 PM - 2:00 PM' ? 'selected' : '' ?? '' }}>
+                                                                1:00 PM - 2:00 PM</option>
+                                                            <option value="1:30 PM - 2:30 PM"
+                                                                {{ isset($order?->orderProductBox?->first()['pickup_time']) == '1:30 PM - 2:30 PM' ? 'selected' : '' ?? '' }}>
+                                                                1:30 PM - 2:30 PM</option>
+                                                            <option value="2:00 PM - 3:00 PM"
+                                                                {{ isset($order?->orderProductBox?->first()['pickup_time']) == '2:00 PM - 3:00 PM' ? 'selected' : '' ?? '' }}>
+                                                                2:00 PM - 3:00 PM</option>
+                                                            <option value="2:30 PM - 3:30 PM"
+                                                                {{ isset($order?->orderProductBox?->first()['pickup_time']) == '2:30 PM - 3:30 PM' ? 'selected' : '' ?? '' }}>
+                                                                2:30 PM - 3:30 PM</option>
+                                                            <option value="3:00 PM - 4:00 PM"
+                                                                {{ isset($order?->orderProductBox?->first()['pickup_time']) == '3:00 PM - 4:00 PM' ? 'selected' : '' ?? '' }}>
+                                                                3:00 PM - 4:00 PM</option>
+                                                            <option value="3:30 PM - 4:30 PM"
+                                                                {{ isset($order?->orderProductBox?->first()['pickup_time']) == '3:30 PM - 4:30 PM' ? 'selected' : '' ?? '' }}>
+                                                                3:30 PM - 4:30 PM</option>
+                                                            <option value="4:00 PM - 5:00 PM"
+                                                                {{ isset($order?->orderProductBox?->first()['pickup_time']) == '4:00 PM - 5:00 PM' ? 'selected' : '' ?? '' }}>
+                                                                4:00 PM - 5:00 PM</option>
+                                                            <option value="4:30 PM - 5:30 PM"
+                                                                {{ isset($order?->orderProductBox?->first()['pickup_time']) == '4:30 PM - 5:30 PM' ? 'selected' : '' ?? '' }}>
+                                                                4:30 PM - 5:30 PM</option>
+                                                            <option value="5:00 PM - 6:00 PM"
+                                                                {{ isset($order?->orderProductBox?->first()['pickup_time']) == '5:00 PM - 6:00 PM' ? 'selected' : '' ?? '' }}>
+                                                                5:00 PM - 6:00 PM</option>
+                                                            <option value="5:30 PM - 6:30 PM"
+                                                                {{ isset($order?->orderProductBox?->first()['pickup_time']) == '5:30 PM - 6:30 PM' ? 'selected' : '' ?? '' }}>
+                                                                5:30 PM - 6:30 PM</option>
+                                                            <option value="6:00 PM - 7:00 PM"
+                                                                {{ isset($order?->orderProductBox?->first()['pickup_time']) == '6:00 PM - 7:00 PM' ? 'selected' : '' ?? '' }}>
+                                                                6:00 PM - 7:00 PM</option>
+                                                            <option value="6:30 PM - 7:30 PM"
+                                                                {{ isset($order?->orderProductBox?->first()['pickup_time']) == '6:30 PM - 7:30 PM' ? 'selected' : '' ?? '' }}>
+                                                                6:30 PM - 7:30 PM</option>
                                                         </select>
                                                     </div>
                                                 </div>
                                                 <div class="col-md-6">
                                                     @if ($order?->status != 'processing')
-                                                        <button id="print_package_submit" type="submit" class="btn btn-success">Confirm & Get
+                                                        <button id="print_package_submit" type="submit"
+                                                            class="btn btn-success">Confirm & Get
                                                             Slip</button>
                                                     @endif
                                                 </div>
                                                 <div class="col-md-6">
-                                                        <a id="print_package_barcode"
-                                                            href="{{ route('package_barcodes.barcodes', ['order' => $order->id]) }}"
-                                                            target="_blank"
-                                                            class="btn btn-outline-primary float-right"  {{$order?->status == 'processing' ? '' : 'hidden'}}>Print</a>
+                                                    <a id="print_package_barcode"
+                                                        href="{{ route('package_barcodes.barcodes', ['order' => $order->id]) }}"
+                                                        target="_blank" class="btn btn-outline-primary float-right"
+                                                        {{ $order?->status == 'processing' ? '' : 'hidden' }}>Print</a>
                                                 </div>
                                             </div>
                                         </div>
@@ -551,8 +589,8 @@
                         @csrf
                         <div class="form-group">
                             <label for="payment_note">Add Note:</label>
-                            <input type="text" class="form-control form-control-sm" id="payment_note"
-                                name="payment_note">
+                                <textarea type="text" class="form-control form-control-sm" id="payment_note" name="payment_note"
+                                placeholder="Add Note" required></textarea>
                         </div>
                         <div class="form-group row">
                             <div class="col-md-8">
@@ -614,8 +652,8 @@
                         id="orderFulfilmentNoteSubmit">
                         <div class="form-group">
                             <label for="fulfilment_note">Add Note:</label>
-                            <input type="text" class="form-control form-control-sm" id="fulfilment_note"
-                                name="fulfilment_note">
+                            <textarea type="text" class="form-control form-control-sm" id="fulfilment_note" name="fulfilment_note"
+                                placeholder="Add Note" required></textarea>
                         </div>
                         <div class="form-group row">
                             <div class="col-md-8">
