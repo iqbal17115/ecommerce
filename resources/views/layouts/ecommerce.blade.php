cdbedda5585d0fa7f81484890008128ebbcafaf1
Md Iqbal Hossain <59364699+iqbal17115@users.noreply.github.com>
2023-10-05 21:43:28 +0600

    Update ecommerce.blade.php

--- resources/views/layouts/ecommerce.blade.php
+++ resources/views/layouts/ecommerce.blade.php
@@ -4,7 +4,12 @@
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <link rel="canonical" href="{{ url()->current() }}">
+    @php
+    $urlWithoutProtocol = preg_replace('/^https?:\/\//', '', Request::root());
+    $currentUrl = 'www.' . $urlWithoutProtocol . Request::getRequestUri();
+@endphp
+
+<link rel="canonical" href="{{ $currentUrl }}">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <title>
         @if ($company_info && $company_info->title)
