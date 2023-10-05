b402bdd4e8962b9d25817e6d74fa30318b5ff0de
Md Iqbal Hossain <59364699+iqbal17115@users.noreply.github.com>
2023-10-05 22:06:20 +0600

    Updated

--- resources/views/layouts/ecommerce.blade.php
+++ resources/views/layouts/ecommerce.blade.php
@@ -4,12 +4,7 @@
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    @php
-        $urlWithoutProtocol = preg_replace('/^https?:\/\//', '', Request::root());
-        $currentUrl = 'www.' . $urlWithoutProtocol . Request::getRequestUri();
-    @endphp
-
-    <link rel="canonical" href="{{ $currentUrl }}">
+    <link rel="canonical" href="{{ url()->current() }}">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <title>
         @if ($company_info && $company_info->title)
