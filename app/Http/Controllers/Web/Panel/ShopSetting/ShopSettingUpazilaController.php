e6bef9b26d8473a79ba3432bb6829d3867a2a5ae
Md Iqbal Hossain <59364699+iqbal17115@users.noreply.github.com>
2023-10-10 01:47:15 +0600

    Update ShopSettingUpazilaController.php

--- app/Http/Controllers/Web/Panel/ShopSetting/ShopSettingUpazilaController.php
+++ app/Http/Controllers/Web/Panel/ShopSetting/ShopSettingUpazilaController.php
@@ -21,11 +21,5 @@ class ShopSettingUpazilaController extends Controller
         $this->isFilterExists = false;
     }
 
-    /**
-     * @throws Exception
-     */
-    public function __invoke(): View|JsonResponse
-    {
-        return $this->generateView($this->viewPath);
-    }
+   
 }
