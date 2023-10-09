269efb14bcde6a170ad313762555144dff9722aa
Md Iqbal Hossain <59364699+iqbal17115@users.noreply.github.com>
2023-10-10 01:53:25 +0600

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -162,7 +162,7 @@ Route::group(['middleware' => 'auth'], function () {
 
 //
 Route::group([], function () {
-    Route::get('admin', [HomeController::class, 'adminDashboard'])->name('dashboard')->middleware(['verified']);
+    Route::get('admin', [HomeController::class, 'adminDashboard'])->name('dashboard')->middleware(['auth:sanctum', 'verified']);
 
     // Shipping
     Route::controller(ShippingChargeController::class)->group(function () {
