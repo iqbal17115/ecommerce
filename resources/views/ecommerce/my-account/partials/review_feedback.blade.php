f72ed976e2b7d53a7edde1f210d220441dc5d8c1
Md Iqbal Hossain <59364699+iqbal17115@users.noreply.github.com>
2023-11-27 21:56:00 +0600

    Update review_feedback.blade.php

--- resources/views/ecommerce/my-account/partials/review_feedback.blade.php
+++ resources/views/ecommerce/my-account/partials/review_feedback.blade.php
@@ -1,5 +1,5 @@
 <div class="tab-pane fade" id="review_feedback">
-    <h2 class="tab-title" style="background-color: #fff;">111</h2>
+    <h2 class="tab-title" style="background-color: #fff;">Review & Feedback</h2>
     <div class="comment-list mb-0 shadow common_bg" id="comment_list"></div>
     <hr class="mt-0 mb-3 pb-2" />
 
