47b9133616f6aa110118e7b2b25bc2d5a6201e5c
Md Iqbal Hossain <59364699+iqbal17115@users.noreply.github.com>
2023-11-28 22:37:12 +0600

    Revert "Update Kernel.php"
    This reverts commit e32c2a4a6ac5fe7a680dbd6d746a14821e189dc1.

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -41,8 +41,8 @@ class Kernel extends HttpKernel
         ],
 
         'api' => [
-            \Illuminate\Session\Middleware\StartSession::class,
-            //\Laravel\Sanctum\Http\Middleware\EnsureFrontendRequestsAreStateful::class,
+            //\Illuminate\Session\Middleware\StartSession::class,
+            \Laravel\Sanctum\Http\Middleware\EnsureFrontendRequestsAreStateful::class,
             'throttle:60,1',
             //\Illuminate\Routing\Middleware\SubstituteBindings::class,
         ],
