8ad5d12c50e18ca698a303f199a2ec6de1049322
Md Iqbal Hossain <59364699+iqbal17115@users.noreply.github.com>
2023-11-30 09:22:53 +0600

    Update AuthenticateCustomer.php

--- app/Http/Middleware/AuthenticateCustomer.php
+++ app/Http/Middleware/AuthenticateCustomer.php
@@ -9,6 +9,7 @@ class AuthenticateCustomer
 {
     public function handle($request, Closure $next)
     {
+        dd(Auth::user());
         // Use the 'auth' middleware instead of checking with Auth::check()
         if (auth()->check()) {
             return $next($request);
