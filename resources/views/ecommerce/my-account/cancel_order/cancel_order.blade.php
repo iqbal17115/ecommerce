9259f2541d485a59527d4f8ef8c92cc9a657cafd
Md Iqbal Hossain <59364699+iqbal17115@users.noreply.github.com>
2023-12-12 20:41:18 +0600

    Updated

--- resources/views/ecommerce/my-account/cancel_order/cancel_order.blade.php
+++ resources/views/ecommerce/my-account/cancel_order/cancel_order.blade.php
@@ -121,35 +121,54 @@
     </main><!-- End .main -->
 @endsection
 @push('scripts')
-    <!-- Include jQuery library if not already included -->
-    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
-    <script src="{{ asset('js/panel/action.js') }}"></script>
-    <script src="{{ asset('js/panel/common.js') }}"></script>
-    <script src="{{ asset('js/select2.js') }}"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery.form/4.3.0/jquery.form.min.js"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-confirm/3.3.2/jquery-confirm.min.js"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.min.js"></script>
-    <script src="{{ asset('js/panel/web/my_account/cancel_order.js') }}"></script>
+    <script src="{{ asset('js/panel/users/cart/cart.js') }}"></script>
+    <script src="{{ asset('js/panel/users/common.js') }}"></script>
     <script>
-        $(document).ready(function() {
-            $('.product-return-qty').keyup(function() {
-                var productId = $(this).data('product_id');
-                var orderedQty = parseFloat($('#previous_quantity' + productId).val());
-                var returnQty = parseFloat($(this).val());
+        window.onload = function() {
+            // Code to be executed after rendering the full layout
+            function lazyLoad() {
+                const lazyImages = document.querySelectorAll('.lazy-load');
+                lazyImages.forEach(img => {
+                    if (img.getBoundingClientRect().top <= window.innerHeight && img.getBoundingClientRect()
+                        .bottom >= 0 && getComputedStyle(img).display !== 'none') {
+                        img.src = img.dataset.src;
+                        img.classList.remove('lazyload');
+                    }
+                });
+            }
 
-                if (isNaN(returnQty)) {
-                    returnQty = 0;
-                    returnQty = null;
-                    remainingQty = null;
-                } else {
-                    var remainingQty = orderedQty - returnQty;
-                }
+            // Check for visible images on page load
+            document.addEventListener("DOMContentLoaded", lazyLoad);
+            // Get an array of all the image elements you want to load
+            var images = document.getElementsByClassName('lazy-load');
 
+            // Set up an IntersectionObserver to detect when the images are in view
+            var options = {
+                rootMargin: '0px',
+                threshold: 0.1
+            };
 
+            var observer = new IntersectionObserver(function(entries, observer) {
+                entries.forEach(function(entry) {
+                    // If the image is in the viewport, load it by setting its `src` attribute to the appropriate URL
+                    if (entry.isIntersecting) {
+                        var image = entry.target;
+                        var imageUrl = image.getAttribute('data-src');
+                        image.src = imageUrl;
+                        image.classList.remove(
+                            'lazy-load'
+                        ); // Remove the class to prevent the image from being loaded again
+                        observer.unobserve(
+                            image); // Stop observing the image once it has been loaded
+                    }
+                });
+            }, options);
 
-                $('#product_order_cancel_qty_' + productId).val(returnQty);
-                $('#new_quantity' + productId).val(remainingQty);
-            });
-        });
+            // Loop through all the image elements and observe them with the IntersectionObserver
+            for (var i = 0; i < images.length; i++) {
+                var image = images[i];
+                observer.observe(image);
+            }
+        };
     </script>
 @endpush
