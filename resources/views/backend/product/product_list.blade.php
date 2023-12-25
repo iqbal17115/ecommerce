644a33be4ae0e547f80c183aa809b55981f2e1ed
Md Iqbal Hossain <59364699+iqbal17115@users.noreply.github.com>
2023-12-25 21:36:37 +0600

    Update product_list.blade.php

--- resources/views/backend/product/product_list.blade.php
+++ resources/views/backend/product/product_list.blade.php
@@ -78,7 +78,7 @@
         <div class="modal-dialog">
             <div class="modal-content">
                 <div class="modal-header">
-                    <h5 class="modal-title mt-0" id="myCountryLabel">Country</h5>
+                    <h5 class="modal-title mt-0" id="myCountryLabel">Product qty</h5>
                     <button type="button" class="close" data-dismiss="modal" aria-label="Close">
                         <span aria-hidden="true">&times;</span>
                     </button>
