1772a54bee8867aeb2c3b4719d4913498df89e49
Md Iqbal Hossain <59364699+iqbal17115@users.noreply.github.com>
2024-01-25 19:45:08 +0600

    Your Payment

--- resources/views/ecommerce/my-account/partials/your_payment.blade.php
+++ resources/views/ecommerce/my-account/partials/your_payment.blade.php
@@ -1,4 +1,114 @@
 <div class="tab-pane fade" id="your_payment">
     <h2 class="tab-title">Your Payment</h2>
 
+    <div class="row">
+        <div class="col-xl-3 col-sm-6 col-12 mb-2">
+            <div class="card">
+                <div class="card-content">
+                    <div class="card-body">
+                        <div class="media d-flex">
+                            <div class="media-body text-left">
+                                <h3 class="success">0.00</h3>
+                                <span>Your Balances</span>
+                            </div>
+                            <div class="align-self-center">
+                                <i class="icon-user success font-large-2 float-right"></i>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div class="col-xl-3 col-sm-6 col-12 mb-2">
+            <div class="card">
+                <div class="card-content">
+                    <div class="card-body">
+                        <div class="media d-flex">
+                            <div class="media-body text-left">
+                                <h3 class="success">0.00</h3>
+                                <span>Your Purchase</span>
+                            </div>
+                            <div class="align-self-center">
+                                <i class="icon-user success font-large-2 float-right"></i>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div class="col-xl-3 col-sm-6 col-12 mb-2">
+            <div class="card">
+                <div class="card-content">
+                    <div class="card-body">
+                        <div class="media d-flex">
+                            <div class="media-body text-left">
+                                <h3 class="success">0.00</h3>
+                                <span>Your Return</span>
+                            </div>
+                            <div class="align-self-center">
+                                <i class="icon-user success font-large-2 float-right"></i>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div class="col-xl-3 col-sm-6 col-12 mb-2">
+            <div class="card">
+                <div class="card-content">
+                    <div class="card-body">
+                        <div class="media d-flex">
+                            <div class="media-body text-left">
+                                <h3 class="success">0.00</h3>
+                                <span>Your Refund</span>
+                            </div>
+                            <div class="align-self-center">
+                                <i class="icon-user success font-large-2 float-right"></i>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div class="col-xl-3 col-sm-6 col-12 mb-2">
+            <div class="card">
+                <div class="card-content">
+                    <div class="card-body">
+                        <div class="media d-flex">
+                            <div class="media-body text-left">
+                                <h3 class="success">0.00</h3>
+                                <span>Your Rewards</span>
+                            </div>
+                            <div class="align-self-center">
+                                <i class="icon-user success font-large-2 float-right"></i>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div class="col-xl-3 col-sm-6 col-12 mb-2">
+            <div class="card">
+                <div class="card-content">
+                    <div class="card-body">
+                        <div class="media d-flex">
+                            <div class="media-body text-left">
+                                <h3 class="success">0.00</h3>
+                                <span>Your Gift Cards</span>
+                            </div>
+                            <div class="align-self-center">
+                                <i class="icon-user success font-large-2 float-right"></i>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+    </div>
 </div>
