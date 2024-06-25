e2074a77416fc7d6e151165c31b554ed69a224cf
Md Iqbal Hossain <59364699+iqbal17115@users.noreply.github.com>
2024-06-25 23:42:04 +0600

    Update UnitController.php

--- app/Http/Controllers/Backend/Product/UnitController.php
+++ app/Http/Controllers/Backend/Product/UnitController.php
@@ -49,6 +49,7 @@ class UnitController extends Controller
             $unit->user_id = Auth::user()->id;
         }
 
+        // Test
         $unit->name = $request->name;
         $unit->short_name = $request->short_name;
         $unit->branch_id = 1;
