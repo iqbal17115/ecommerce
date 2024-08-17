00ddddc7f2588726f08f7f742d6267924e9e4879
Md Iqbal Hossain <59364699+iqbal17115@users.noreply.github.com>
2024-08-17 21:50:08 +0600

    Update shop.blade.php

--- resources/views/ecommerce/shop.blade.php
+++ resources/views/ecommerce/shop.blade.php
@@ -381,10 +381,10 @@
                             <img class="lazy-load" data-src="${product.image_url}" width="239" height="239" alt="product">
                         </a>
                         ${product.is_offer_active ? `
-                                            <div class="label-group">
-                                                <div class="product-label label-sale">-${product.offer_percentage}%</div>
-                                            </div>
-                                        ` : ''}
+                                                    <div class="label-group">
+                                                        <div class="product-label label-sale">-${product.offer_percentage}%</div>
+                                                    </div>
+                                                ` : ''}
                         <div class="btn-icon-group">
                             <a href="javascript:void(0);" data-product_id="${product.id}" class="btn-icon add_cart_item product-type-simple">
                                 <i class="icon-shopping-cart"></i>
@@ -408,8 +408,8 @@
                         </div>
                         <div class="price-box">
                             ${product.is_offer_active ? `
-                                                <span class="old-price">${product.active_currency.icon}${product.your_price}</span>
-                                            ` : ''}
+                                                        <span class="old-price">${product.active_currency.icon}${product.your_price}</span>
+                                                    ` : ''}
                             <span class="product-price">${product.active_currency.icon}${product.is_offer_active ? product.sale_price : product.your_price}</span>
                         </div>
                     </div>
@@ -507,18 +507,34 @@
         //     }
         // });
 
-        function lazyLoad() {
-            var images = document.querySelectorAll("img[data-src]");
-            images.forEach(function(img) {
-                if (img.getBoundingClientRect().top < window.innerHeight && img.getBoundingClientRect().bottom >
-                    0) {
-                    img.src = img.dataset.src;
-                    img.removeAttribute("data-src");
-                    console.log("Image loaded:", img.src);
-                }
-            });
-        }
+        window.onload = function() {
+            function lazyLoad() {
+                const images = document.querySelectorAll('.lazy-load');
 
-        document.addEventListener("DOMContentLoaded", lazyLoad);
+                const options = {
+                    rootMargin: '0px',
+                    threshold: 0.1
+                };
+
+                const observer = new IntersectionObserver((entries, observer) => {
+                    entries.forEach(entry => {
+                        if (entry.isIntersecting) {
+                            const img = entry.target;
+                            const imageUrl = img.getAttribute('data-src');
+                            img.src = imageUrl;
+                            img.classList.remove('lazy-load');
+                            observer.unobserve(img);
+                        }
+                    });
+                }, options);
+
+                images.forEach(img => {
+                    observer.observe(img);
+                });
+            }
+
+            // Execute lazyLoad after the full layout is rendered
+            lazyLoad();
+        };
     </script>
 @endpush
