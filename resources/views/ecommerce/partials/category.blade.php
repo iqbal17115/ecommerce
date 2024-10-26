432880956fc9ffb687b5f8f1e4d354e4f5bc86d8
Md Iqbal Hossain <59364699+iqbal17115@users.noreply.github.com>
2024-10-26 17:42:21 +0600

    Category Design

--- resources/views/ecommerce/partials/category.blade.php
+++ resources/views/ecommerce/partials/category.blade.php
@@ -1,13 +1,10 @@
-<li>
+<li class="category-item {{ isset($flag) ? 'list-group-item':''}}">
     @if ($category->subcategories->isNotEmpty())
-        <a href="#widget-category-{{ $category->id }}" class="collapsed load-subcategories"
-           data-toggle="collapse"
-           data-id="{{ $category->id }}"
-           data-category="{{ $category->name }}"
-           role="button"
-           aria-expanded="false"
+        <a href="#widget-category-{{ $category->id }}" class="category-link load-subcategories" data-toggle="collapse"
+           data-id="{{ $category->id }}" data-category="{{ $category->name }}" role="button" aria-expanded="false"
            aria-controls="widget-category-{{ $category->id }}">
-            {{ $category->name }} <span class="toggle"></span>
+            {{ $category->name }}
+            <span class="toggle-icon">&#9662;</span>
         </a>
         <div class="collapse" id="widget-category-{{ $category->id }}">
             <ul class="cat-sublist" id="sublist-{{ $category->id }}">
@@ -15,7 +12,7 @@
             </ul>
         </div>
     @else
-        <a href="javascript:void(0);" class="category-filter" data-category="{{ $category->name }}">
+        <a href="javascript:void(0);" class="category-link category-filter" data-category="{{ $category->name }}">
             {{ $category->name }}
         </a>
     @endif
--- resources/views/ecommerce/shop/partials/category_widget.blade.php
+++ resources/views/ecommerce/shop/partials/category_widget.blade.php
@@ -1,20 +1,17 @@
-<div class="widget">
+<div class="widget categories-widget">
     <h3 class="widget-title">
-        <a data-toggle="collapse" href="#widget-body-2" role="button" aria-expanded="true"
-            aria-controls="widget-body-2">Categories</a>
+        <a data-toggle="collapse" href="#widget-body-2" role="button" aria-expanded="true" aria-controls="widget-body-2">
+            Categories
+            <span class="widget-icon">&#9662;</span>
+        </a>
     </h3>
-
     <div class="collapse show" id="widget-body-2">
         <div class="widget-body">
-            <ul class="cat-list">
+            <ul class="cat-list list-group">
                 @foreach ($categories as $category)
-                    @include('ecommerce.partials.category', [
-                        'category' => $category,
-                    ])
+                    @include('ecommerce.partials.category', ['category' => $category, 'flag' => 1])
                 @endforeach
             </ul>
         </div>
-        <!-- End .widget-body -->
     </div>
-    <!-- End .collapse -->
 </div>
