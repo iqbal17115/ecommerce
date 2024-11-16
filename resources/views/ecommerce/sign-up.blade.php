9f06ec962b2dc5ada9b2c44870f3316083f1e032
Md Iqbal Hossain <iqbal86571361@gmail.com>
2024-11-16 14:18:29 +0600

    Register Page Design Update

--- resources/views/ecommerce/sign-up.blade.php
+++ resources/views/ecommerce/sign-up.blade.php
@@ -1,65 +1,136 @@
 <!DOCTYPE html>
-<html>
+<html lang="en">
 
 <head>
-    <title>Register</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"
-        integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
-    <link rel="stylesheet" type="text/css" href="{{ asset('css/web/sign_up.css') }}">
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Register</title>
+  <style>
+    body {
+      background-color: #f4631b;
+      font-family: "Segoe UI", Tahoma, Geneva, Verdana, sans-serif;
+    }
 
+    .form1 {
+      background: #ffffff;
+      margin-top: 0px;
+      height: 100%;
+      border-radius: 15px;
+      box-shadow: 0px 4px 15px rgba(0, 0, 0, 0.2);
+    }
+
+    .form1 form {
+      position: absolute;
+      top: 40%;
+      left: 50%;
+      transform: translate(-50%, -50%);
+      box-shadow: 1px 1px 15px rgba(0, 0, 0, 0.15);
+      backdrop-filter: blur(4px);
+      -webkit-backdrop-filter: blur(4px);
+      border-radius: 15px;
+      padding: 20px;
+      width: 340px;
+      background: #f7f7f7;
+    }
+
+    .form1 label {
+      display: block;
+      margin-bottom: 5px;
+      font-weight: bold;
+    }
+
+    .form1 label span.required {
+      color: red;
+      font-size: 1.2em;
+      margin-left: 2px;
+    }
+
+    .form1 input,
+    .form1 select {
+      display: block;
+      width: 95%;
+      height: 30px;
+      padding: 10px;
+      margin-bottom: 15px;
+      border: 1px solid #ccc;
+      border-radius: 20px;
+      font-size: 1rem;
+      transition: all 450ms;
+    }
+
+    .form1 select {
+      text-align: center;
+    }
+
+    .form1 input[type="radio"] {
+      font-size: 14px;
+      size: 10px;
+      height: 12px;
+    }
+
+    .form1 button {
+      height: 40px;
+      width: 100%;
+      border-radius: 25px;
+      font-weight: 700;
+      font-family: "Segoe UI", Tahoma, Geneva, Verdana, sans-serif;
+      font-size: 1.2rem;
+      color: #fff;
+      cursor: pointer;
+      transition: all 450ms;
+      background: #f4631b;
+      border: none;
+    }
+
+    .form1 button:hover {
+      background-color: #d85316;
+    }
+  </style>
 </head>
 
 <body>
+  <div class="main">
+    <div class="form1">
+      <form method="POST" action="{{ route('customer-register') }}" id="checkout-form">
+        @csrf
+        <label for="name">
+          Name <span class="required">*</span>
+        </label>
+        <input type="text" name="name" id="name" placeholder="Enter Full Name" required>
 
-    <!-- Add your signup form here -->
-    <div class="row">
-        <div class="col-md-12">
-            <div class="signup-form">
-                <div class="container">
-                    <div class="header">
-                        <h1>Create an Account</h1>
-                        @if (session('message'))
-                            <div class="alert-message">
-                                {{ session('message') }}
-                            </div>
-                        @endif
+        <label for="mobile">
+          Number <span class="required">*</span>
+        </label>
+        <input type="text" name="mobile" id="mobile" placeholder="Enter Mobile Number" required>
 
-                    </div>
-                    <form method="POST" action="{{ route('customer-register') }}" id="checkout-form">
-                        @csrf
-                        <div class="input">
-                            <i class="fa-solid fa-user"></i>
-                            <input type="text" name="name" placeholder="Name" />
-                        </div>
-                        <div class="input">
-                            <i class="fa-solid fa-envelope"></i>
-                            <input type="text" name="mobile" placeholder="Mobile" />
-                        </div>
-                        <div class="input">
-                            <i class="fa-solid fa-lock"></i>
-                            <input type="password" name="password" placeholder="Password" />
-                        </div>
-                        <input class="signup-btn" type="submit" value="SIGN UP" />
-                    </form>
-                    <p>Already have an account <a href="{{ route('customer-sign-in') }}">sign in</a></p>
-                </div>
-            </div>
-        </div>
+        <label for="email">
+          Email
+        </label>
+        <input type="email" name="email" id="email" placeholder="Enter Email Address">
+
+        <label for="gender">
+          Gender
+        </label>
+        <select id="gender" name="gender">
+          <option value="" disabled selected>Select Gender</option>
+          <option value="male">Male</option>
+          <option value="female">Female</option>
+        </select>
+
+        <label for="bday">
+          Date Of Birth
+        </label>
+        <input type="date" name="bday" id="bday">
+
+        <label for="password">
+          Password <span class="required">*</span>
+        </label>
+        <input type="password" name="password" id="password" placeholder="Password" required>
+
+        <button class="btn" type="submit">Register</button>
+      </form>
     </div>
-
-    <!-- Option 1: Bootstrap Bundle with Popper -->
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"
-        integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous">
-    </script>
-
-    <!-- Option 2: Separate Popper and Bootstrap JS -->
-    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js"
-        integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous">
-    </script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js"
-        integrity="sha384-cVKIPhGWiC2Al4u+LWgxfKTRIcfu0JTxR+EQDz/bgldoEyl4H0zUF0QKbrJ0EcQF" crossorigin="anonymous">
-    </script>
-
+  </div>
 </body>
 
 </html>
