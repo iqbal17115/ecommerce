40dcbe1dbb033eedc2f7bdfe8c8934ad035b8ac5
Md Iqbal Hossain <iqbal86571361@gmail.com>
2024-11-18 08:23:07 +0600

    Register Page Design Update

--- app/Http/Middleware/Authenticate.php
+++ app/Http/Middleware/Authenticate.php
@@ -14,6 +14,7 @@ class Authenticate extends Middleware
      */
     protected function redirectTo($request)
     {
+        // When not sign in then redirect
         if (!$request->expectsJson()) {
             return route('customer-sign-in');
         }
