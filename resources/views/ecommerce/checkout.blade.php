0d75e2690a3f7b3d00cfafbe88e81ab3b71a7807
Md Iqbal Hossain <iqbal86571361@gmail.com>
2024-12-06 11:43:29 +0600

    Default product dimension 0

--- resources/views/ecommerce/checkout.blade.php
+++ resources/views/ecommerce/checkout.blade.php
@@ -177,9 +177,10 @@
     <!-- End Shipping Address Modal -->
 @endsection
 @push('scripts')
-    <script src="{{ asset('js/panel/users/checkout/address.js') }}"></script>
-    <script src="{{ asset('js/panel/users/cart/checkout_page_cart.js') }}"></script>
-    <script src="{{ asset('js/panel/users/common.js') }}"></script>
+    <script src="{{ asset('js/panel/users/checkout/address.js') }}?v={{ time() }}"></script>
+    <script src="{{ asset('js/panel/users/cart/checkout_page_cart.js') }}?v={{ time() }}"></script>
+    <script src="{{ asset('js/panel/users/common.js') }}?v={{ time() }}"></script>
+
 
     <script>
         var user = <?php echo json_encode($user); ?>;
