15f6542b43544ed33e982a67f664e304eac811a1
iqbal171 <iqbal86571361@gmail.com>
2025-01-11 04:07:07 -0700

    Resolved merge conflicts in .ftp-deploy-sync-state.json, .ftpquota, and sign-in.blade.php

--- resources/views/ecommerce/sign-in.blade.php
+++ resources/views/ecommerce/sign-in.blade.php
@@ -210,11 +210,15 @@ input[type="password"] {
 <<<<<<< Updated upstream
 <<<<<<< Updated upstream
 <<<<<<< Updated upstream
+<<<<<<< Updated upstream
         <div style="position: relative;">
           <input type="password" name="password" id="password" placeholder="Enter Password" autocomplete="password" required />
           <i class="bi bi-eye-slash" id="togglePassword" style="position: absolute; right: 10px; top: 50%; transform: translateY(-50%); cursor: pointer;"></i>
         </div>
 
+=======
+        <input type="password" name="password" placeholder="Enter Password" autocomplete="password" required/>
+>>>>>>> Stashed changes
 =======
         <input type="password" name="password" placeholder="Enter Password" autocomplete="password" required/>
 >>>>>>> Stashed changes
