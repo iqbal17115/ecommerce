43d61f22d9a41cbcbe6d103e480fd67d15e64495
Md Iqbal Hossain <iqbal86571361@gmail.com>
2025-01-12 21:19:45 +0600

    edit address

--- app/Http/Controllers/FrontEnt/LoginController.php
+++ app/Http/Controllers/FrontEnt/LoginController.php
@@ -9,7 +9,7 @@ use Illuminate\Support\Facades\Auth;
 class LoginController extends Controller
 {
     public function index() {
-        return view('ecommerce.sign-in');
+        return view('ecommerce.sign_in');
     }
     public function authenticate(Request $request)
     {
