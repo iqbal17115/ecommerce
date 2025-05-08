d7c99e971493bf9ce2b3105fc332bc53729f2939
iqbal17115 <iqbal86571361@gmail.com>
2025-05-08 23:23:52 +0600

    place order

--- resources/views/layouts/parts/css-links.blade.php
+++ resources/views/layouts/parts/css-links.blade.php
@@ -1,19 +1,9 @@
-<!-- Preload critical CSS -->
-<link rel="preload" href="{{ URL::asset('aladdinne/') }}/assets/css/bootstrap.min.css" as="style" onload="this.onload=null;this.rel='stylesheet'">
-<link rel="preload" href="{{ URL::asset('aladdinne/') }}/assets/css/demo36.min.css" as="style" onload="this.onload=null;this.rel='stylesheet'">
-<link rel="preload" href="{{ URL::asset('aladdinne/') }}/assets/vendor/fontawesome-free/css/all.min.css" as="style" onload="this.onload=null;this.rel='stylesheet'">
-<link rel="preload" href="{{ asset('web_css/global.css') }}" as="style" onload="this.onload=null;this.rel='stylesheet'">
+<!-- Load essential layout styles normally -->
+<link rel="stylesheet" href="{{ URL::asset('aladdinne/') }}/assets/css/bootstrap.min.css">
+<link rel="stylesheet" href="{{ URL::asset('aladdinne/') }}/assets/css/demo36.min.css">
+<link rel="stylesheet" href="{{ URL::asset('aladdinne/') }}/assets/vendor/fontawesome-free/css/all.min.css">
+<link rel="stylesheet" href="{{ asset('web_css/global.css') }}">
 
-<!-- Non-critical stylesheets: loaded async -->
+<!-- Defer non-critical styles -->
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/jquery-confirm/3.3.2/jquery-confirm.min.css" media="print" onload="this.media='all'">
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.min.css" media="print" onload="this.media='all'">
-
-<!-- Fallback for users with JavaScript disabled -->
-<noscript>
-    <link rel="stylesheet" href="{{ URL::asset('aladdinne/') }}/assets/css/bootstrap.min.css">
-    <link rel="stylesheet" href="{{ URL::asset('aladdinne/') }}/assets/css/demo36.min.css">
-    <link rel="stylesheet" href="{{ URL::asset('aladdinne/') }}/assets/vendor/fontawesome-free/css/all.min.css">
-    <link rel="stylesheet" href="{{ asset('web_css/global.css') }}">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/jquery-confirm/3.3.2/jquery-confirm.min.css">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.min.css">
-</noscript>
