8acccfe57270415428e07c8ad3c88bd8dab1e0e8
iqbal17115 <iqbal86571361@gmail.com>
2025-05-08 23:18:55 +0600

    place order

--- resources/views/layouts/parts/css-links.blade.php
+++ resources/views/layouts/parts/css-links.blade.php
@@ -1,11 +1,19 @@
-    <!-- Plugins CSS File -->
-    <link rel="stylesheet" href="{{ URL::asset('aladdinne/') }}/assets/css/bootstrap.min.css">
+<!-- Preload critical CSS -->
+<link rel="preload" href="{{ URL::asset('aladdinne/') }}/assets/css/bootstrap.min.css" as="style" onload="this.onload=null;this.rel='stylesheet'">
+<link rel="preload" href="{{ URL::asset('aladdinne/') }}/assets/css/demo36.min.css" as="style" onload="this.onload=null;this.rel='stylesheet'">
+<link rel="preload" href="{{ URL::asset('aladdinne/') }}/assets/vendor/fontawesome-free/css/all.min.css" as="style" onload="this.onload=null;this.rel='stylesheet'">
+<link rel="preload" href="{{ asset('web_css/global.css') }}" as="style" onload="this.onload=null;this.rel='stylesheet'">
 
-    <!-- Main CSS File -->
+<!-- Non-critical stylesheets: loaded async -->
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/jquery-confirm/3.3.2/jquery-confirm.min.css" media="print" onload="this.media='all'">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.min.css" media="print" onload="this.media='all'">
+
+<!-- Fallback for users with JavaScript disabled -->
+<noscript>
+    <link rel="stylesheet" href="{{ URL::asset('aladdinne/') }}/assets/css/bootstrap.min.css">
     <link rel="stylesheet" href="{{ URL::asset('aladdinne/') }}/assets/css/demo36.min.css">
-    <link rel="stylesheet" type="text/css"
-        href="{{ URL::asset('aladdinne/') }}/assets/vendor/fontawesome-free/css/all.min.css">
-    <link rel="stylesheet" type="text/css" href="{{ asset('web_css/global.css') }}">
-    <script type="text/javascript" src="https://translate.google.com/translate_a/element.js?cb=googleTranslateElementInit"></script>
+    <link rel="stylesheet" href="{{ URL::asset('aladdinne/') }}/assets/vendor/fontawesome-free/css/all.min.css">
+    <link rel="stylesheet" href="{{ asset('web_css/global.css') }}">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/jquery-confirm/3.3.2/jquery-confirm.min.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.min.css">
+</noscript>
