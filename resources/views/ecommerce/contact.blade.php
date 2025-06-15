e7dfe25fd7c8daa424b5a2e4ad82dca1bf6af93e
iqbal17115 <iqbal86571361@gmail.com>
2025-06-15 22:28:11 +0600

    shop page

--- resources/views/ecommerce/contact.blade.php
+++ resources/views/ecommerce/contact.blade.php
@@ -270,5 +270,28 @@
 
 			<div class="mb-8"></div>
 		</main>
-
 @endsection
+@push('scripts')
+<script src="{{ asset('js/panel/users/common.js') }}?v={{ time() }}"></script>
+<script src="{{ asset('js/panel/users/cart/add_to_cart.js') }}?v={{ time() }}"></script>
+<script src="{{ asset('js/panel/users/cart/cart_manager.js') }}?v={{ time() }}"></script>
+<script src="{{ asset('js/panel/users/cart/cart_drawer.js') }}?v={{ time() }}"></script>
+<script src="{{ asset('js/panel/users/cart/cart_list.js') }}?v={{ time() }}"></script>
+<script>
+    // Set the hasCartList variable
+    window.hasCartList = false;
+    // Add an event listener to the DOMContentLoaded event
+    document.addEventListener('DOMContentLoaded', function() {
+        if (typeof CartManager !== 'undefined') {
+            CartManager.loadCartData();
+        }
+    });
+
+    // This handles BACK button cache restore
+    window.addEventListener('pageshow', function(event) {
+        if (event.persisted) {
+            CartManager.loadCartData();
+        }
+    });
+</script>
+@endpush
