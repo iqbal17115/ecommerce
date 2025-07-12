9f8c2486022638cf827ce5f647321c932eb8e395
iqbal17115 <iqbal86571361@gmail.com>
2025-07-12 19:41:36 +0600

    Update Status

--- app/Http/Controllers/Ecommerce/HomeController.php
+++ app/Http/Controllers/Ecommerce/HomeController.php
@@ -90,7 +90,7 @@ class HomeController extends Controller
         $top_show_categories = Category::whereTopMenu(1)->whereIsActive(1)->orderByRaw('ISNULL(position), position ASC')->get();
         $product_features = ProductFeature::getAllLists($this->homePageService->getProductFeatures(), [], HomePageProductFeatureResource::class);
         $top_features = ProductFeature::with('TopFeatureSetting', 'TopFeatureSetting.FeatureSettingDetail', 'TopFeatureSetting.FeatureSettingDetail.Category', 'TopFeatureSetting.ProductFeature', 'TopFeatureSetting.ProductFeature.Advertisement', 'Product')->whereCardFeature(1)->whereTopMenu(1)->whereIsActive(1)->orderByRaw('ISNULL(position), position ASC')->get();
-        dd(1);
+        dd(2);
         return view('ecommerce.home', compact(['sliders', 'top_show_categories', 'product_features', 'top_features', 'user_id']));
     }
 }
