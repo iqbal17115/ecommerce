c9a856aacdb30868962e4045996e0ccf41c15294
iqbal17115 <iqbal86571361@gmail.com>
2025-07-12 19:30:02 +0600

    Update Status

--- app/Http/Controllers/Ecommerce/HomeController.php
+++ app/Http/Controllers/Ecommerce/HomeController.php
@@ -88,9 +88,9 @@ class HomeController extends Controller
         $sliders = $this->cacheService->remember('home_sliders', function () {
             return ;
         }, 300); // cache 5 mins
-
-        $top_show_categories = Category::whereTopMenu(1)->whereIsActive(1)->orderByRaw('ISNULL(position), position ASC')->get();
                         dd(3);
+        $top_show_categories = Category::whereTopMenu(1)->whereIsActive(1)->orderByRaw('ISNULL(position), position ASC')->get();
+
         $product_features = ProductFeature::getAllLists($this->homePageService->getProductFeatures(), [], HomePageProductFeatureResource::class);
 
         $top_features = ProductFeature::with('TopFeatureSetting', 'TopFeatureSetting.FeatureSettingDetail', 'TopFeatureSetting.FeatureSettingDetail.Category', 'TopFeatureSetting.ProductFeature', 'TopFeatureSetting.ProductFeature.Advertisement', 'Product')->whereCardFeature(1)->whereTopMenu(1)->whereIsActive(1)->orderByRaw('ISNULL(position), position ASC')->get();
