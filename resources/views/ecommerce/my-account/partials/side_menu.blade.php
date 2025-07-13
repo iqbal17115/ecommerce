c29768cc04d6b1f4e244754d7a0677ef794e7e15
iqbal17115 <iqbal86571361@gmail.com>
2025-07-14 05:49:06 +0600

    Update Status

--- resources/views/ecommerce/my-account/partials/side_menu.blade.php
+++ resources/views/ecommerce/my-account/partials/side_menu.blade.php
@@ -6,83 +6,83 @@
             </a>
         </li> -->
         <li class="nav-item">
-            <a class="nav-link text-danger" href="javascript:void(0);">
+            <a class="nav-link font-weight-bold text-danger" href="javascript:void(0);">
                 <i class="fas fa-sign-out-alt" style="visibility: hidden;"></i> Edit Profile
             </a>
         </li>
         <li class="nav-item">
-            <a class="nav-link text-danger" href="/customer-logout">
+            <a class="nav-link font-weight-bold text-danger" href="/customer-logout">
                <i class="fas fa-sign-out-alt" style="visibility: hidden;"></i> Sign Out
             </a>
         </li>
         <li class="nav-item">
-            <a class="nav-link" href="#orders" data-toggle="tab">
-                <i class="fas fa-box-open"></i> Your Orders
+            <a class="nav-link font-weight-bold" href="#orders" data-toggle="tab">
+                <i class="fas fa-box-open" style="color: #f4631b;"></i> My Orders
             </a>
         </li>
         <li class="nav-item">
-            <a class="nav-link" href="#address" data-toggle="tab">
-                <i class="fas fa-map-marker-alt"></i> Your Address
+            <a class="nav-link font-weight-bold" href="#address" data-toggle="tab">
+                <i class="fas fa-map-marker-alt" style="color: #f4631b;"></i> My Address
             </a>
         </li>
         <li class="nav-item">
-            <a class="nav-link" href="#order_status" data-toggle="tab">
-                <i class="fas fa-clipboard-check"></i> Order Status
+            <a class="nav-link font-weight-bold" href="#order_status" data-toggle="tab">
+                <i class="fas fa-clipboard-check" style="color: #f4631b;"></i> Order Status
             </a>
         </li>
         <li class="nav-item">
-            <a class="nav-link" href="#mobile-app" data-toggle="tab">
-                <i class="fas fa-mobile-alt"></i> Mobile App
+            <a class="nav-link font-weight-bold" href="#mobile-app" data-toggle="tab">
+                <i class="fas fa-mobile-alt" style="color: #f4631b;"></i> Mobile App
             </a>
         </li>
         <li class="nav-item">
-            <a class="nav-link cart-item-tab">
-                <i class="fas fa-shopping-cart"></i> Cart
+            <a class="nav-link font-weight-bold cart-item-tab">
+                <i class="fas fa-shopping-cart" style="color: #f4631b;"></i> My Cart
             </a>
         </li>
         <li class="nav-item">
-            <a class="nav-link @if ($type == 'wishlist') active @endif wishlist-item-tab">
-                <i class="fas fa-heart"></i> Wishlist
+            <a class="nav-link font-weight-bold @if ($type == 'wishlist') active @endif wishlist-item-tab">
+                <i class="fas fa-heart" style="color: #f4631b;"></i> Wishlist
             </a>
         </li>
         <li class="nav-item">
-            <a class="nav-link" href="#reward-gift-card" data-toggle="tab">
-                <i class="fas fa-gift"></i> Reward & Gift Card
+            <a class="nav-link font-weight-bold" href="#reward-gift-card" data-toggle="tab">
+                <i class="fas fa-gift" style="color: #f4631b;"></i> Reward & Gift Card
             </a>
         </li>
         <li class="nav-item">
-            <a class="nav-link" href="#your_payment" data-toggle="tab">
-                <i class="fas fa-credit-card"></i> Your Payment
+            <a class="nav-link font-weight-bold" href="#your_payment" data-toggle="tab">
+                <i class="fas fa-credit-card" style="color: #f4631b;"></i> My Payment
             </a>
         </li>
         <li class="nav-item">
-            <a class="nav-link" href="#your_transactions" data-toggle="tab">
-                <i class="fas fa-receipt"></i> Transactions
+            <a class="nav-link font-weight-bold" href="#installation-plan" data-toggle="tab">
+                <i class="fas fa-tools" style="color: #f4631b;"></i> Installation Plan
             </a>
         </li>
         <li class="nav-item">
-            <a class="nav-link" href="#installation-plan" data-toggle="tab">
-                <i class="fas fa-tools"></i> Installation Plan
+            <a class="nav-link font-weight-bold" href="#review_feedback" data-toggle="tab">
+                <i class="fas fa-comment-dots" style="color: #f4631b;"></i> Review & Feedback
             </a>
         </li>
         <li class="nav-item">
-            <a class="nav-link" href="#return_exchange" data-toggle="tab">
-                <i class="fas fa-exchange-alt"></i> Return & Exchange
+            <a class="nav-link font-weight-bold" href="#your_transactions" data-toggle="tab">
+                <i class="fas fa-receipt" style="color: #f4631b;"></i> Transactions
             </a>
         </li>
         <li class="nav-item">
-            <a class="nav-link" href="#review_feedback" data-toggle="tab">
-                <i class="fas fa-comment-dots"></i> Review & Feedback
+            <a class="nav-link font-weight-bold" href="#return_exchange" data-toggle="tab">
+                <i class="fas fa-exchange-alt" style="color: #f4631b;"></i> Return & Exchange
             </a>
         </li>
         <li class="nav-item">
-            <a class="nav-link" href="#my-account-contact-us" data-toggle="tab">
-                <i class="fas fa-envelope-open-text"></i> Contact Us
+            <a class="nav-link font-weight-bold" href="#my-account-contact-us" data-toggle="tab">
+                <i class="fas fa-envelope-open-text" style="color: #f4631b;"></i> Contact Us
             </a>
         </li>
         <li class="nav-item">
-            <a class="nav-link" href="#switch-accounts" data-toggle="tab">
-                <i class="fas fa-user-friends"></i> Switch Accounts
+            <a class="nav-link font-weight-bold" href="#switch-accounts" data-toggle="tab">
+                <i class="fa-solid fa-right-left" style="color: #f4631b;"></i> Switch Accounts
             </a>
         </li>
     </ul>
