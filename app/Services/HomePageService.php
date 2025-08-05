608123e49dd7dc5b5c7f74bf9b1ed7a608c9d7a5
iqbal17115 <iqbal86571361@gmail.com>
2025-08-05 15:34:30 +0600

    Cache Isse

--- app/Services/HomePageService.php
+++ app/Services/HomePageService.php
@@ -8,6 +8,6 @@ class HomePageService
 {
     public function getProductFeatures()
     {
-        return ProductFeature::with('Category', 'Product', 'Product.ProductMainImage', 'Product.productVariations', 'Product.reviewSum')->has('Product')->whereCardFeature(0)->whereTopMenu(0)->whereIsActive(1)->orderByRaw('ISNULL(position), position ASC');
+        return ProductFeature::with('Category', 'Product', 'Product.ProductMainImage', 'Product.productVariations', 'Product.reviewSum')->has('Product')->whereCardFeature(0)->whereTopMenu(0)->whereIsActive(1)->limit(5)->orderByRaw('ISNULL(position), position ASC');
     }
 }
