87b9e3794a767fe7395cf6ac5ff6aebefd1d73f3
iqbal17115 <iqbal86571361@gmail.com>
2025-09-16 19:23:49 +0600

    Speed

--- resources/views/layouts/parts/css-links.blade.php
+++ resources/views/layouts/parts/css-links.blade.php
@@ -5,5 +5,6 @@
 <link rel="stylesheet" href="{{ asset('web_css/global.css') }}">
 
 <!-- Defer non-critical styles -->
-<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/jquery-confirm/3.3.2/jquery-confirm.min.css" media="print" onload="this.media='all'">
-<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.min.css" media="print" onload="this.media='all'">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/jquery-confirm/3.3.2/jquery-confirm.min.css" media="print" onload="this.media='all'" onerror="this.media='all'">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.min.css" media="print" onload="this.media='all'" onerror="this.media='all'">
+
