15c1ac4b7639f0bdf3a04146a2bfcc3ab226dbf2
iqbal17115 <iqbal86571361@gmail.com>
2025-10-02 12:43:40 +0600

    shipping charge

--- app/Http/Resources/User/Home/ProductFeature/HomePageProductFeatureResource.php
+++ app/Http/Resources/User/Home/ProductFeature/HomePageProductFeatureResource.php
@@ -17,7 +17,7 @@ class HomePageProductFeatureResource extends JsonResource
     {
         return [
             'name' => $this->name,
-            'products' => ProductListResource::collection($this->Product)->resolve(),
+            'products' => ProductListResource::collection($this->Product->take(3))->resolve(),
         ];
     }
 }
