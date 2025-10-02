4e8493df14eeee793f773f6cf41ae2c52cc98f9f
iqbal17115 <iqbal86571361@gmail.com>
2025-10-02 12:15:17 +0600

    shipping charge

--- app/Services/HomePageService.php
+++ app/Services/HomePageService.php
@@ -10,8 +10,7 @@ class HomePageService
     {
         return ProductFeature::with(['Category', 
          'Product' => function ($query) {
-            $query->whereIsActive(1) // optional
-                  ->latest()        // order by created_at desc
+            $query->latest()        // order by created_at desc
                   ->take(8);        // limit to 8 products per feature
         },
         'Product.ProductMainImage', 'Product.productVariations', 'Product.reviewSum'])->whereCardFeature(0)->whereTopMenu(0)->whereIsActive(1)->orderByRaw('ISNULL(position), position ASC');
